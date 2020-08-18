83c1df3bdcc6a7d0d69d158d0e05d6f59649f154
Luiz Felipe Carvalho Libertino <raziel_libertino@hotmail.com>
2020-08-18 13:16:50 -0400

    finished challenge

--- index.js
+++ index.js
@@ -2,27 +2,35 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
-  await page.goto('https://instagram.com/rocketseat_oficial');
+  await page.goto('https://am.olx.com.br/regiao-de-manaus');
   
-  const imgList = await page.evaluate(() => {
+  const titlesAndImg = await page.evaluate(() => {
     //toda essa função será executada no browser
 
     // vamos pegar todas as imagens que estão na parte de posts
-    const nodeList = document.querySelectorAll('article img')
+    const nodeList = document.querySelectorAll('#ad-list li a')
+    const nodeListImg = document.querySelectorAll('#ad-list li a div div div img');
     //transformar o NodeList em array
-    const imgArray = [...nodeList]
+    const titlesList = [...nodeList]
+    const listImgs = [...nodeListImg]
     // transformar os nodes (elementos html) em objetos JS
-    const imgList = imgArray.map( ({src}) => ({
-      src
-    }))
+    const imgs = listImgs.map( html => ({ src: html.dataset.src !==  undefined ? html.dataset.src : html.src}));
+    const titlesAndImg = titlesList
+      .filter(({title}) => title !== "")
+      .map( ({title}, index) => ({
+      title,
+      src: imgs[index].src || ''
+    }));
+    
     // colocar para fora da função
-    return imgList
+    return titlesAndImg
+
   })
 
   // escrever os dados em um arquivo local (json)
-  fs.writeFile('instagram.json', JSON.stringify(imgList, null, 2), err => {
+  fs.writeFile('olx.json', JSON.stringify(titlesAndImg, null, 2), err => {
     if (err) throw new Error( 'something went wrong');
 
     console.log('well done!');
